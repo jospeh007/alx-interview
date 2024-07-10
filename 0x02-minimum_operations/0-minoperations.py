f95bc66314997fa2a2ee32dd07d1b848b38583c8
Youssef Rhaimouni <youssefgf123@gmail.com>
2024-07-10 09:34:50 -0700

    Update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,17 +4,14 @@
 
 def minOperations(n: int) -> int:
     """ Return an integer """
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
-        else:
-            op += 1
-            body += next
-    if len(body) != n:
+    if n < 2:
         return 0
-    return op
+    factor_list = []
+    i = 1
+    while n != 1:
+        i += 1
+        if n % i == 0:
+            while n % i == 0:
+                n /= i
+                factor_list.append(i)
+    return sum(factor_list)
