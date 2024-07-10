2db0c233f480e3c5bd50e1f22b8e1ee32c2bccef
Youssef Rhaimouni <youssefgf123@gmail.com>
2024-07-10 09:05:04 -0700

    Task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Minimum Operations """
+
+
+def minOperations(n)
+""" Return an integer """
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
+        else:
+            op += 1
+            body += next
+    if len(body) != n:
+        return 0
+    return op
